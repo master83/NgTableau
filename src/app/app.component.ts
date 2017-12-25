0b6c1f0ea9802bf021a1fc2664fce32f6db4c736
Rahul Kumar <rforrahul83@gmail.com>
2017-12-25 18:03:02 -0500

    Basic Angular + Tableau integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var tableau: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  tableauViz: any;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    const placeholderDiv = document.getElementById('tableauViz');
+    const url = 'https://public.tableau.com/views/USTreasuryInterestRate/Sheet1?:embed=y&:display_count=yes';
+    const options = {
+      hideTabs: true,
+      width: '800px',
+      height: '700px',
+      onFirstInteractive: function () {
+        // The viz is now ready and can be safely used.
+      }
+    };
+    this.tableauViz = new tableau.Viz(placeholderDiv, url, options);
+  }
 }
